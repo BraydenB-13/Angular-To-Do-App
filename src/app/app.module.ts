b3238dd0df156c784515674fea3eff7293c940ad
Brayden Barger <brayden.barger8273@stu.mtec.edu>
2021-03-04 08:24:16 -0700

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { MainBodyComponent } from './main-body/main-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolBarComponent,
+    MainBodyComponent
   ],
   imports: [
     BrowserModule,
